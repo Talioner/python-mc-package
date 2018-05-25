e4cded2c187aa4163a37bfba61eecdfc0e5242ad
Talioner <226021@student.pwr.edu.pl>
2018-05-25 09:23:14 +0200

    Added commnets

--- mc_package/materials_counter.py
+++ mc_package/materials_counter.py
@@ -1,8 +1,12 @@
 def count_tiles(x, y, s):
+    """Returns amount of tiles needed for the specified room size."""
     result = float(x) * float(y) / float(s)
     return result
 
 
 def count_time(s, v):
+    """Returns time needed to reach the destination,
+    having distance and speed from input.
+    """
     time = float(s) / float(v)
     return time
